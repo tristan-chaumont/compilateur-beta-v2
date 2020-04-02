4dd718ee4165d98086ef380077b488ff3e8fce21
Tristan CHAUMONT <tristan.chaumont9@etu.univ-lorraine.fr>
2020-04-02 14:55:44 +0200

    Method genererProgramme into Generateur class

--- projet/compilateur/src/main/java/fr/ul/miage/groupe/g/generateur/Generateur.java
+++ projet/compilateur/src/main/java/fr/ul/miage/groupe/g/generateur/Generateur.java
@@ -1,14 +1,23 @@
 package fr.ul.miage.groupe.g.generateur;
 
 import fr.ul.miage.arbre.Noeud;
+import fr.ul.miage.groupe.g.main.StringBuilderPlus;
 import fr.ul.miage.tds.Tds;
 
 public class Generateur {
 
 
 	public String genererProgramme(Noeud noeud, Tds tds) {
-		StringBuilder builder = new StringBuilder("");
-		builder.append("");
-		return builder.toString();
+		StringBuilderPlus builder = new StringBuilderPlus(".include beta.uasm", true);
+		builder.appendLine(".include intio.uasm");
+		builder.appendLine(".options tty");
+		builder.appendLine("");
+		builder.appendLine("CMOVE(pile, SP)");
+		builder.appendLine(genererData(tds));
+		builder.appendLine(genererCode(noeud));
+		builder.appendLine("debut:");
+		builder.appendLineTab("CALL(main)");
+		builder.appendLineTab("HALT");
+		builder.appendLine("pile:");
 	}
 }
--- projet/compilateur/src/main/java/fr/ul/miage/groupe/g/main/StringBuilderPlus.java
+++ projet/compilateur/src/main/java/fr/ul/miage/groupe/g/main/StringBuilderPlus.java
@@ -8,6 +8,14 @@ public class StringBuilderPlus {
         this.stringBuilder = new StringBuilder();
     }
     
+    /**
+     * Constructeur qui prend une valeur T ainsi qu'un booléen valant true s'il faut sauter une ligne (appendLine) après la valeur ou non.
+     * Ce constructeur évite de devoir instancier le constructeur vide puis de faire un append / appendLine pour ajouter une première valeur.
+     * @param t
+     * 		Valeur à ajouter dans le builder.
+     * @param lineSeparator
+     * 		Booléen valant true s'il faut sauter une ligne ou non.
+     */
     public <T> StringBuilderPlus(T t, boolean lineSeparator) {
     	if(lineSeparator) {
         	this.stringBuilder = new StringBuilder();
@@ -22,9 +30,19 @@ public class StringBuilderPlus {
         stringBuilder.append(t);
         return this;
     }
+    
+    public <T> StringBuilderPlus appendTab(T t) {
+        stringBuilder.append("\t").append(t);
+        return this;
+    }
 
     public <T> StringBuilderPlus appendLine(T t) {
         stringBuilder.append(t).append(System.lineSeparator());
+        return this;
+    }
+    
+    public <T> StringBuilderPlus appendLineTab(T t) {
+        stringBuilder.append("\t").append(t).append(System.lineSeparator());
         return this;
     }
 
