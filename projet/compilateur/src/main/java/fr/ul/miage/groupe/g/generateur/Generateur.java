7cc92b1e1e40c39453527bed1bc67fb071c3f4e2
Tristan CHAUMONT <tristan.chaumont9@etu.univ-lorraine.fr>
2020-04-06 10:04:11 +0200

    Empty

--- projet/compilateur/src/main/java/fr/ul/miage/groupe/g/generateur/Generateur.java
+++ projet/compilateur/src/main/java/fr/ul/miage/groupe/g/generateur/Generateur.java
@@ -96,7 +96,7 @@ public class Generateur {
 		StringBuilderPlus builder = new StringBuilderPlus();
 		builder.appendLine(String.format("%s:", noeudObj.getValeur()));
 		builder.appendLine(genererBloc(noeud));
-		builder.appendLineTab("RTN²()");
+		builder.appendLineTab("RTN()");
 		return builder.toString();
 	}
 	
